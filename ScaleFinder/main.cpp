067523960571f341a4f3c8fba2d81df2343d8b4e
Jabeer Ahmed <jabeer.ahmed@intel.com>
2017-01-10 16:34:40 -0800

    Initial Commit

---
+++ ScaleFinder/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ScaleFinder
+//
+//  Created by Jabeer Ahmed (Intel) on 1/10/17.
+//  Copyright © 2017 Me. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
